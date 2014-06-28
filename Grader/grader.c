a5b19b46c597aaf79cbc134e7fb4708eddd0baca
Alexoner <onerhao@gmail.com>
2014-06-28 11:13:36 +0800

    remove main function

--- Grader/grader.c
+++ Grader/grader.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 int AdditionTest(int a, int b);
-int main() {
-    int a = 0, b = 0;
-    while (scanf("%d %d", &a, &b) == 2) {
-        printf("%d\n", AdditionTest(a, b));
-    }
-    return 0;
-}
+
 
 int AdditionTest(int a, int b)
 {
