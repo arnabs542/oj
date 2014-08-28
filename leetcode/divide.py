55efcdf133e3b74d5937d995579d90ad9d015dd7
Alexoner <onerhao@gmail.com>
2014-08-28 16:32:08 +0800

    leetcode:binary method solution for Divide Two Integers Problem

---
+++ leetcode/divide.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+
+'''
+
+Divide two integers without using multiplication,division and mod operator
+
+SOLUTION:
+    division(multiplication) by 2 can be implemented by right(left)
+    shifting by 1.
+
+    Any number can be represented 2's power.
+
+'''
+
+
+class Solution:
+    # @return an integer
+
+    def divide(self, dividend, divisor):
+        quotient = 0
+        a = abs(dividend)
+        order = 0
+        b = abs(divisor)
+        c = b
+        sign = (dividend < 0) ^ (divisor < 0)
+        # print "sign is :{}".format(sign)
+        while a >= b:
+            order = 0
+            c = b
+            while (c << 1) <= a:
+                order += 1
+                c = c << 1
+            a -= (b << order)
+            quotient += 1 << order
+
+        if sign > 0:
+            return -quotient
+        else:
+            return quotient
+
+if __name__ == "__main__":
+    print Solution().divide(4, -2)
+    print Solution().divide(4, 1)
+    print Solution().divide(4, 3)
+    print Solution().divide(34359738368, 1)
