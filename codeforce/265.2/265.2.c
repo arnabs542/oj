4df1aba42adfe0d71e1d10d63a1a476f49926e49
Alexoner <onerhao@gmail.com>
2014-09-08 12:02:16 +0800

    codeforces:return 0 in main function,but a TLE error still there

--- codeforce/265.2/265.2.c
+++ codeforce/265.2/265.2.c
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
         {
             /*printf("YES:\n");*/
             printf("%s\n", newText);
-            return 1;
+            return 0;
         }
     }
 
