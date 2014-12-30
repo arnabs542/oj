ccc125dc1086cfb9be7bb37baff512737cde7cbc
onerhao <onerhao@gmail.com>
2014-12-30 16:49:09 +0800

    leetcode: use hash table and enumerating adjacent words to reduce time complexity

--- leetcode/WordLadder.java
+++ leetcode/WordLadder.java
@@ -2,24 +2,34 @@
  * Word Ladder
  *
  * Given two words (start and end), and a dictionary, find the length of shortest transformation sequence from start to end, such that:
-
-Only one letter can be changed at a time
-Each intermediate word must exist in the dictionary
-For example,
-
-Given:
-start = "hit"
-end = "cog"
-dict = ["hot","dot","dog","lot","log"]
-As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
-return its length 5.
-
-Note:
-Return 0 if there is no such transformation sequence.
-All words have the same length.
-All words contain only lowercase alphabetic characters.
+ *
+ * Only one letter can be changed at a time
+ * Each intermediate word must exist in the dictionary
+ * For example,
+ *
+ * Given:
+ * start = "hit"
+ * end = "cog"
+ * dict = ["hot","dot","dog","lot","log"]
+ * As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+ * return its length 5.
+ *
+ * Note:
+ * Return 0 if there is no such transformation sequence.
+ * All words have the same length.
+ * All words contain only lowercase alphabetic characters.
  *
  *
+ * Algorithm:
+ * This is a shortest path problem in Graph theory.If the Edit Distance between two words is the distance 
+ * between two vertices representing them.
+ * For shorest path problem, we have breadth-fist search problem.
+ *      Naively,we can build the graph with time complexity of O(|V|*|V|),and do a BFS in O(|V|+|E|).This 
+ * implementation will fail the big data test.To optimize the code,we can get around the graph building process:
+ * We store vertices in a hash table,thus retrieving them in constant time.Initially,we push the start word into 
+ * the queueWhile Breadth-first searching,do change one character each time with a word,and check if it's in the 
+ * dictionary.If it's in,then we push it into a queue,updating its search depth.Then we can find the shortest path
+ * in O(|V| * L * 26).L is the words' length.
  */
 
 
@@ -36,6 +46,7 @@ public class WordLadder {
 
     public int length;
 
+    // Check if two words are 1 distance from each other,no used actually
     public boolean isConnected(String sa, String sb) {
         int length = 0;
         if (sa.length() != sb.length()) {
@@ -54,31 +65,44 @@ public class WordLadder {
     }
 
     public int ladderLength(String start, String end, Set<String> dict) {
-        // TODO Auto-generated constructor stub
         Map<String, Integer> ladderLength = Collections.synchronizedMap(new HashMap<String, Integer>());
         int length;
-
         for (String word : dict) {
             ladderLength.put(word, Integer.MAX_VALUE);
         }
 
-        // the ladder length from start to start is 0
+        // the ladder length from start to start is 0.
+        // Initialization
         ladderLength.put(start, 1);
         Queue<String> queue = new LinkedList<String>();
-        // Breadth-first search to solve this graph shortest path problem
-        /*
-         * if (dict.contains(start)) { // string start is in the dictionary,ladder length is 0 return 0; }
-         */
         queue.offer(start);
         dict.add(end);
         ladderLength.put(end, Integer.MAX_VALUE);
+        
+        // maintenance
         while (!queue.isEmpty()) {
             String word = queue.poll();
+            // termination
             if (word.equals(end)) {
                 length = ladderLength.get(word);
                 return length;
             }
-            // System.out.println(word);
+            
+            int l = word.length();
+            for (int i = 0; i < l; i++) {
+                char[] arr = word.toCharArray();
+                for (char c = 'a'; c <= 'z'; c++) {
+                    arr[i] = c;
+                    String newWord = new String(arr);
+                    if (dict.contains(newWord) && !newWord.equals(word)) {
+                        dict.remove(newWord);
+                        queue.offer(newWord);
+                        ladderLength.put(newWord, ladderLength.get(word) + 1);
+                        System.out.println(ladderLength);
+                    }
+                }
+            }
+            /*
             for (String adjString : dict) {
                 if (this.isConnected(word, adjString) && ladderLength.get(word) < ladderLength.get(adjString)) {
                     ladderLength.put(adjString, ladderLength.get(word) + 1);
@@ -94,16 +118,18 @@ public class WordLadder {
                     // dict.remove(adjString);
                 }
             }
+            */
         }
-        length = -1;
-        return length;
+        return 0;
     }
 
     static public void main(String[] args) {
         Set<String> set = new HashSet<String>(Arrays.asList("hot", "dot", "lot", "log"));
         // System.out.println(set);
-        LadderLength ll = new LadderLength("hit", "cog", set);
+        WordLadder ll = new WordLadder();
         System.out.println(ll.ladderLength("hit", "cog", set));
+        
+        set = new HashSet<String>(Arrays.asList("a","b","c"));
+        System.out.println(ll.ladderLength("a","c"));
     }
-
 }
