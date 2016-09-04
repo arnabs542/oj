f6e13305b6c13bf7534251879d626d54d8f752fa
Alexoner <onerhao@gmail.com>
2016-09-04 18:32:37 +0800

    permutations: dynamic programming

--- leetcode/permutations.py
+++ leetcode/permutations.py
@@ -24,7 +24,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        solutions = self.permuteBacktrack(nums)
+        # solutions = self.permuteBacktrack(nums)
+        solutions = self.permuteDP(nums)
         return solutions
 
     def permuteDP(self, nums):
@@ -35,8 +36,25 @@ class Solution(object):
         Dynamic Programming solution to permutations problem
         state transition relationship:
             permutations[n] = n * permutations[n - 1]
+
+        performance: 98.06%, 2016-09-04 18:32 CST
         """
-        pass
+        if not hasattr(self, 'permutations'):
+            self.permutations = []
+
+        if nums:
+            self.permutations.append([[nums[0]]])
+            for i in range(1, len(nums)):
+                permutations_i = []
+                self.permutations.append(permutations_i)
+                # import ipdb
+                # ipdb.set_trace()
+                for permutation_previous in self.permutations[i - 1]:
+                    for j in range(i + 1):
+                        permutation = list(permutation_previous)
+                        permutation.insert(j, nums[i])
+                        permutations_i.append(permutation)
+        return self.permutations[-1]
 
     def permuteBacktrack(self, nums, start=0):
         """
@@ -79,6 +97,7 @@ class Solution(object):
 def test():
     nums = [1, 2, 3]
     print(Solution().permuteBacktrack(nums))
+    print(Solution().permuteDP(nums))
 
 if __name__ == '__main__':
     test()
