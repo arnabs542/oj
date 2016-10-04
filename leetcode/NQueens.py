4e1f9bd8148990505903eeaaa7d4973b2f07a756
Alexoner <onerhao@gmail.com>
2016-10-04 15:31:35 +0800

    leetcode, NQueens: reduce nested statements depth

--- leetcode/NQueens.py
+++ leetcode/NQueens.py
@@ -33,19 +33,23 @@ invalid.
 "queens", where i = queen[j] is the column index of the queen in row j. Under
 this representation, the way to generate all candidates is just like generate
 all the permutation of vector [0...n-1].
+
     The key point to optimize is how to handle diagonal coordinates, which we
 can tackle by utilizing ANALYTIC GEOMETRY. For diagonal line like forward slash,
 the equation is y = x + k, where (x, y) is the coordinate and k is an constant.
 For backward slash like diagonal lines, the equation is y = -x + k, (x, y, k)
 are defined the same before. To wrap it up:
+
     (1) The constant of a column is its index range from 0 to n-1.
     (2) The constant of a back slash line "" is "col + row" or "x + y"
     (3) The constant of a forward slash line "/" is "col - row" or "x - y"
+This way we can reject a candidate in O(1) time since we can use three hash tables
+or three boolean arrays as the dynamic sets we need.
 
+Takeaway:
     @1: BACKTRACK with recursive depth-first search.
     @2: BACKTRACK with STACK implemented DFS.
     @3: Bit manipulation instead of array representation of queens.
-    @4:
 
 '''
 
@@ -97,23 +101,19 @@ class Solution(object):
         # backward[0] = False
         # forward[0] = False
 
-        col = 0
+        row = col = 0
 
         while True:
             # TODO(DONE): decouple the STACK's PUSH and POP operation
             row = len(stack)
             if row < n and col < n:
                 # STACK PUSH: place a new queen
-                while col < n:
-                    # if self.check(stack, row, col):
-                    if columns[col] and backward[col + row] and forward[col - row]:
-                        stack.append(col)
-                        columns[col] = backward[col + row] = forward[col - row] = False
-                        col = 0
-                        break
-                    else:
-                        col += 1
-
+                # check condition satisfied or not
+                if columns[col] and backward[col + row] and forward[col - row]:
+                    stack.append(col)
+                    columns[col] = backward[col + row] = forward[col - row] = False
+                    col = 0
+                    continue
             else:
                 if col >= n:
                     # search has met its dead end, and maybe
@@ -121,18 +121,21 @@ class Solution(object):
                     if not row:
                         return solutions
                 else:
-                    # row gets no less than n: a solution has already been found
-                    # stack size is the board width, i.e., a solution has been found
+                    # row gets no less than n: stack size is the board width,
+                    # i.e., a solution has been found
                     solutions.append(Solution.show(stack, n))
                 # STACK POP
                 col = stack.pop()
                 row -= 1
+                # restore states to BACKTRACK
                 columns[col] = backward[col + row] = forward[col - row] = True
-                col += 1
+            col += 1
 
 
 if __name__ == "__main__":
-    result = Solution().solveNQueens(4)
-    print('{} queens solution: '.format(4), result)
-    result = Solution().solveNQueens(8)
-    print('{} queens with {} solutions'.format(8, len(result)))
+    N = 4
+    result = Solution().solveNQueens(N)
+    print('{} queens solution: '.format(N), result)
+    N = 8
+    result = Solution().solveNQueens(N)
+    print('{} queens with {} solutions'.format(N, len(result)))
--- leetcode/NQueensII.py
+++ leetcode/NQueensII.py
@@ -13,54 +13,51 @@ Solution:
 
 '''
 
-import math
 
+class Solution(object):
 
-class Solution:
-
-    @classmethod
-    def check(cls, stack, row, col):
-        for i in range(0, row, 1):
-            # colinear or diagonal
-            if stack[i] == col or math.fabs(stack[i] - col) == row - i:
-                return False
-
-        return True
-
-    # @return a list of lists of string
     def totalNQueens(self, n):
         """
         :type n: int
-        :rtype: int
+        :rtype: int, n queens on n x n board
         """
 
         num_solutions = 0
         stack = []
-        stack.append(0)
-        candidate_start = 0
+
+        columns = [True] * n # col, range is [0, n - 1]
+        backward = [True] *2 * n # col + row, range is [0, 2n - 2]
+        forward = [True] * 2 * n # col - row, range is [-(n - 1), n - 1]
+
+        row = col = 0
+
         while True:
             # TODO(DONE): decouple the STACK's PUSH and POP operation
-            stack_size = len(stack)
-            if stack_size < n and candidate_start < n:
+            row = len(stack)
+            if row < n and col < n:
                 # STACK PUSH: place a new queen
-                while candidate_start < n:
-                    if self.check(stack, len(stack), candidate_start):
-                        stack.append(candidate_start)
-                        candidate_start = 0
-                        break
-                    else:
-                        candidate_start += 1
-
+                # check condition satisfied or not
+                if columns[col] and backward[col + row] and forward[col - row]:
+                    stack.append(col)
+                    columns[col] = backward[col + row] = forward[col - row] = False
+                    col = 0
+                    continue
             else:
-                # search has met its dead end
-                if candidate_start >= n:
-                    if not stack_size:
+                if col >= n:
+                    # search has met its dead end, and maybe
+                    # the whole depth-firsts search has ended
+                    if not row:
                         return num_solutions
                 else:
-                    # stack size is the board width, i.e., a solution has been found
+                    # row gets no less than n: stack size is the board width,
+                    # i.e., a solution has been found
                     num_solutions += 1
                 # STACK POP
-                candidate_start = stack.pop() + 1
+                col = stack.pop()
+                row -= 1
+                # restore states to BACKTRACK
+                columns[col] = backward[col + row] = forward[col - row] = True
+            col += 1
 
 if __name__ == "__main__":
     results = Solution().totalNQueens(8)
