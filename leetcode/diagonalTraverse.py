48828f4222a81a7d4fcd49211263039fc651c0a1
Alexoner <onerhao@gmail.com>
2018-04-20 17:45:31 +0800

    diagonal traverse: state machine

---
+++ leetcode/diagonalTraverse.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+498. Diagonal Traverse
+
+Given a matrix of M x N elements (M rows, N columns), return all elements of the matrix in diagonal order as shown in the below image.
+
+Example:
+Input:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+Output:  [1,2,4,7,5,3,6,8,9]
+Explanation:
+
+Note:
+The total number of elements of the given matrix will not exceed 10,000.
+
+
+================================================================================
+
+1. State machine
+Maintain a state of:(
+    x coordinate,
+    y coordinate,
+    traversing direction,
+)
+
+"""
+
+class Solution:
+    def findDiagonalOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        result = self._findDiagonalOrder(matrix)
+
+        print(matrix, " => ", result)
+
+        return result
+
+    def _findDiagonalOrder(self, matrix):
+        LEFT, RIGHT = 0, 1
+        result = []
+
+        if not matrix or not matrix[0]:
+            return result
+        m, n = len(matrix), len(matrix[0])
+
+        # initialization
+        i = 0
+        x, y = 0, 0
+        direction = RIGHT
+
+        while i < m * n:
+            result.append(matrix[x][y])
+            i += 1
+
+            # next state
+            if direction == RIGHT:
+                if x - 1 >= 0 and y + 1 < n:
+                    x -= 1
+                    y += 1
+                else:
+                    direction = LEFT
+                    if y + 1 < n:
+                        y += 1
+                    else:
+                        x += 1
+
+            elif direction == LEFT:
+                if x + 1 < m and y - 1 >= 0:
+                    x += 1
+                    y -= 1
+                else:
+                    direction = RIGHT
+                    if x + 1 < m:
+                        x += 1
+                    else:
+                        y += 1
+
+        return result
+
+    # TODO: more concise solution
+
+
+def test():
+    solution = Solution()
+
+    assert solution.findDiagonalOrder([
+    ]) == []
+    assert solution.findDiagonalOrder([
+        [1, 2, 3, 4]
+    ]) == [1, 2, 3, 4]
+    assert solution.findDiagonalOrder([
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+    ]) == [1, 2, 5, 6, 3, 4, 7, 8]
+
+    assert solution.findDiagonalOrder([
+        [ 1, 2, 3 ],
+        [ 4, 5, 6 ],
+        [ 7, 8, 9 ],
+    ]) == [1, 2, 4, 7, 5, 3, 6, 8, 9]
+
+    print("self test passed!")
+
+if __name__ == '__main__':
+    test()
--- leetcode/happyNumber.py
+++ leetcode/happyNumber.py
@@ -23,16 +23,34 @@ Example: 19 is a happy number
 6^2 + 8^2 = 100
 1^2 + 0^2 + 0^2 = 1
 
-==============================================================================================
+================================================================================
 SOLUTION
+
+This is a CYCLE DETECTION problem.
 
 1. Naive solution
 Check duplicate until sum equal to 1 with HASH TABLE.
 Space complexity: O(m).
 
-2. Cycle detection: Floyd's tortoise and hare algorithm
+2. CYCLE DETECTION - Floyd's tortoise and hare algorithm
 Space complexity: O(1).
 
+################################################################################
+FOLLOW UP
+1. Why would there be a CYCLE in such sequence?
+The iterated function f is to compute the sum of squares of digits,
+inputs and outputs an integer.
+
+The largest positive integer is 2³² - 1 = 4294967296 - 1. So there are
+at most 10 digits in an positive integer.
+
+For function f, its domain is [0, 2³² - 1], and its value RANGE is finite,
+with upper bound as: 10* 9²=810, since the largest digit is 9.
+
+So function f maps any integer to a finite range [1, 810]. According to
+PIGEONHOLE PRINCIPLE, there must be an duplicate in the iterated sequence.
+In another word, there must be a loop. And 1 is a special loop containing
+only 1 element, where f(1) = 1.
 
 '''
 
@@ -43,13 +61,16 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        # return self.isHappyHash(n)
-        return self.isHappyFloydCycleDetection(n)
+        # result = self._isHappyHash(n)
+        result = self._isHappyFloydCycleDetection(n)
+        print(n, result)
 
-    def isHappyHash(self, n):
+        return result
+
+    def _isHappyHash(self, n):
         visited = {n}
         while n != 1:
-            n = int(sum(map(lambda x: int(x) ** 2, str(n))))
+            n = int(sum(map(lambda x: int(x) ** 2, str(n)))) # sum of squares of digits
             if n in visited:
                 return False
             else:
@@ -58,26 +79,27 @@ class Solution(object):
         print(n, 'is a happy number')
         return True
 
-    def isHappyFloydCycleDetection(self, n):
+    def _isHappyFloydCycleDetection(self, n):
         """
         :type n: int
         :rtype: bool
         """
-        # TODO(done): floyd cycle detection algorithm
-        def transit(x):
+        # DONE: floyd cycle detection algorithm
+        def f(x):
+            '''returns sum of squares of digits'''
             result = 0
             while x:
                 result += (x % 10) ** 2
                 x //= 10
             return result
-        fast = slow = transit(n)
-        fast = transit(fast)
+        slow = f(n)
+        fast = f(slow)
         while fast not in (1, slow):
-            slow = transit(slow)
-            fast = transit(transit(fast))
+            slow = f(slow)
+            fast = f(f(fast))
         return fast == 1
 
-    def isHappyMathClosedForm(self, n):
+    def _isHappyMathClosedForm(self, n):
         # TODO: closed form mathematical solution?
         pass
 
