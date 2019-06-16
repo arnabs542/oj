36995acda27d79867c2fd0ec4576e73e92340a9a
Alexoner <onerhao@gmail.com>
2019-06-16 22:35:45 +0800

    prove two pointers with binary search tree, set theory

--- leetcode/containerWithMostWater.py
+++ leetcode/containerWithMostWater.py
@@ -21,12 +21,12 @@ SOLUTION
 1. A brute-force solution would be compute all pair-wise container volume, time complexity
 O(N*N) = O(N²).
 
-2. Two pointers
+2. Two pointers(GRAPH/binary tree search)
 The target indices i, j must satisfy 0 <= i, j <= n - 1, where n is the size of array.
 Initialize state = (i = 0, j = n - 1), and do graph search inward.
 
 Harnessing the (short plate theory)bucket effect:
-  the SHORT PLATE is the limiting factor of VOLUME of a bucket rather than the longer ones.
+the SHORT PLATE is the limiting factor of VOLUME of a bucket rather than the longer ones.
 
 Illustrating in a matrix representation:
 
@@ -41,7 +41,7 @@ We start by computing the volume at (1,6), denoted by 'o'. Now if the left line 
 than the right line, then all the elements left to (1,6) on the first row have smaller volume,
 so we don't need to compute those cases (crossed by ---).
 
-  1 2 3 4 5 6
+1 2 3 4 5 6
 1 x ------- o
 2 x x
 3 x x x
@@ -52,7 +52,7 @@ so we don't need to compute those cases (crossed by ---).
 Next we move the left line and compute (2,6). Now if the right line is shorter, all cases
 below (2,6) are eliminated.
 
-  1 2 3 4 5 6
+1 2 3 4 5 6
 1 x ------- o
 2 x x       o
 3 x x x     |
@@ -62,13 +62,36 @@ below (2,6) are eliminated.
 And no matter how this o path goes, we end up only need to find the max value on this path,
 which contains n-1 cases.
 
-  1 2 3 4 5 6
+1 2 3 4 5 6
 1 x ------- o
 2 x x - o o o
 3 x x x o | |
 4 x x x x | |
 5 x x x x x |
 6 x x x x x x
+
+This graph is like a binary search tree traversing from (0, n-1) leftward and downward.
+And the solution can be proved by SET THEORY, like in "two sum II".
+
+Complexity: O(n), O(1)
+
+3. Stack (monotonicity problem)
+
+Simply the situation:
+1) Monotonically increasing
+This is like inverse decreasing situation, which can be calculated in O(N),
+by fixing the left boundary to the first which is also largest number.
+
+2) Monotonically decreasing
+O(N) calculation.
+
+3) General case
+A sequence is made of multiple monotonically increasing or decreasing sequence.
+Keep a stack?
+
+No clue yet.
+
+
 '''
 
 class Solution(object):
@@ -78,6 +101,11 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        result = self.maxAreaTwoPointers(height)
+
+        return result
+
+    def maxAreaTwoPointers(self, height):
         i, j = 0, len(height) - 1
         max_volume = 0
         while i < j:
@@ -93,10 +121,14 @@ class Solution(object):
 
         return max_volume
 
+    def maxAreaStack(self, height):
+        pass
+
 def test():
     solution = Solution()
 
     assert solution.maxArea([1, 1]) == 1
+    assert solution.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]) == 49
 
     print('self test passed')
 
--- leetcode/twoSumII.py
+++ leetcode/twoSumII.py
@@ -29,7 +29,11 @@ inverted index: (key=element, value=element's position/index)
 
 Complexity: O(N), O(N)
 
-2. Two pointers
+2. Binary search
+
+Complexity: O(NlogN), O(1)
+
+3. Two pointers - Graph search, ordered tree, pruning
 Think this as a graph search process, but with some pruning process with some greedy strategy.
 
 The target indices i, j must satisfy 0 <= i, j <= n - 1, where n is the length of the array.
@@ -42,11 +46,38 @@ If sum of two pointers' value is less than the target value, then increment i by
 If sum of two pointers' value is greater than the target value, then decrease j by 1.
 Repeat until we have the sum of two pointers value equal to target value.
 
+PROOF
+-----
+
+This is similar to "container with most water".
+
+. 1 2 3 4 5 6
+1 x ------- o
+2 x x - o o o
+3 x x x o | |
+4 x x x x | |
+5 x x x x x |
+6 x x x x x x
+
+The solution correctness can be proved with SET THEORY.
+- The combination coordinates compose of vertices set of a GRAPH.
+- The graph is a CONNECTED COMPONENT itself: each pair of vertices are connected by paths.
+- At each vertex (i, j) there are four directions/edges to go:
+    (i-1, j), (i+1, j), (i, j-1), (i, j+1).
+- At each vertex (i, j), left branch {(x, j)| x < i} yields smaller result,
+    and below/right branch {(x, j)| x > j} yields larger result.
+    This has a similar structure to binary search tree, because the array is ORDERED.
+- Start traversing with (0, n - 1), as the root of the TREE, then this tree
+    is like a BINARY SEARCH TREE.
+    1) Every node can be reached from the ROOT by traversing leftward and downward.
+    2) To not traverse backward, only left and below directions are followed.
+    3) At each time without a match of target sum, the of left/below branch of set
+    of vertices are PRUNED.
+    4) Then finally we traverse to the target point, because graph SET is composed
+    of the solution set and non-solution set, and non-solution set is PRUNED.
+
 Complexity: O(N), O(1)
 
-3. Binary search
-
-Complexity: O(NlogN), O(1)
 
 '''
 class Solution(object):
