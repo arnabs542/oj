d5e9e78e7cdf6336f78597f184bbc48c626b2af4
Alexoner <onerhao@gmail.com>
2019-12-06 12:47:14 +0800

    1d max interval less than k: use state max ending here

--- leetcode/subarrayProductLessThanK.py
+++ leetcode/subarrayProductLessThanK.py
@@ -19,15 +19,18 @@ Note:
 0 < nums[i] < 1000.
 0 <= k < 10^6.
 
-==============================================================================================
 SOLUTION
+================================================================================
 
 1. Brute force
 Exhaust all possible cases, and verify.
 
+With cumulative product calculation, complexity is reduced from O(N³) to O(N²).
 Complexity: O(N²)
 
-2. Cumulative product with binary search
+2. Brute force optimized with binary search
+
+Build a data structure for O(1) range query, and do binary search on that.
 
 Range product query, we can build the prefix product (cumulative product) table, just like
 prefix sum.
@@ -45,6 +48,13 @@ perform binary search to find the lower bound!
 Complexity: O(NlogN)
 
 But, how about overflow?
+1) Use logarithm to transform product to addition
+Overflow can be tackled with logarithm operation:
+\log(\prod(xi)) = \sum(\log(xi))
+
+Multiplication is transformed to addition with logarithm.
+
+2) Check for overflow manually
 
 3. Dynamic programming
 Still O(N²)
@@ -52,15 +62,27 @@ Still O(N²)
 4. States ending here
 Keep track of sum ending here, that is smaller than k. Keep it in a dictionary.
 
-But this degenerates to brute force method for [2, 2, 2, 2, 2], k = 10^6 case.
+But this degenerates to brute force method for [2, 2, 2, 2, 2], k = 10^6 case,
+since we don't need to keep track of all sums, we can just find the intervals and count!
 
 5. Sliding window
+
+To find all globally optimal, we can keep track of all locally optimal intervals.
+
+Keep track of state (i, j, p) = (start index of window, end index of window, eligible product),
+denoting longest subarray ending here,  with product less than k.
+
+State transition:
+    Similar to max subarray problem, shrink left or expand right end of the window.
 
 Find maximum windows, and compute the valid subarrays.
 
 For each right, call opt(right) the smallest left so that the product of the subarray
 nums[left] * nums[left + 1] * ... * nums[right] is less than k.
 opt is a MONOTONE INCREASING FUNCTION, so we can use a SLIDING WINDOW.
+
+Complexity:
+O(N)
 
 
 """
@@ -73,7 +95,8 @@ class Solution:
         :rtype: int
         """
         # result = self._numSubarrayProductLessThanKSlidingWindow(nums, k)
-        result = self._numSubarrayProductLessThanKSlidingWindow2(nums, k)
+        # result = self._numSubarrayProductLessThanKSlidingWindow2(nums, k)
+        result = self._numSubarrayProductLessThanKSlidingWindowEndingHere(nums, k)
 
         print(nums, k, result)
 
@@ -99,6 +122,9 @@ class Solution:
         return count
 
     def _numSubarrayProductLessThanKSlidingWindow2(self, nums, k):
+        """
+        This is an approach to compute from beginning index
+        """
         count = 0
         i, j = 0, 0 # range pointers
         product = 1 # initialization: range product of [i, ..., j - 1], maybe not than k
@@ -116,12 +142,29 @@ class Solution:
 
         return count
 
+    def _numSubarrayProductLessThanKSlidingWindowEndingHere(self, nums, k):
+        """
+        This is an approach to compute from end index
+        """
+        count = 0
+        i, j = 0, 0 # (optimal) sliding window start index, end index
+        product = 1 # product of sliding window ending at j
+        for j in range(len(nums)):
+            while product * nums[j] >= k and i < j:
+                product //= nums[i]
+                i += 1 # shrink
+            product *= nums[j] # expand, product ending here
+            if product < k: count += j - i + 1
+
+        return count
+
 
 def test():
     solution = Solution()
 
     assert solution.numSubarrayProductLessThanK([], 1) == 0
     assert solution.numSubarrayProductLessThanK([], 2) == 0
+    assert solution.numSubarrayProductLessThanK([1], 0) == 0
     assert solution.numSubarrayProductLessThanK([1], 3) == 1
     assert solution.numSubarrayProductLessThanK([1, 1], 3) == 3
     assert solution.numSubarrayProductLessThanK([1, 3, 1], 4) == 6
